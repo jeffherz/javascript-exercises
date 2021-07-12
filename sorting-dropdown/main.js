6bc260637bd75bf50fd0466029ce1b2c92464638
JJHerz <jeffherz565@gmail.com>
2021-07-12 18:27:14 -0400

    Started work on sorting dropdown menu

---
+++ sorting-dropdown/main.js
@@ -0,0 +1,5 @@
+/* For the JavaScript, if the dropdown changes, a function would see if an option was selected
+    Then, you would show the next question id based on the previous one (if college was selected, you would see the more in depth college question)
+
+    Once every question was answered, the function would see the last tiered question and show the links that have the same id.
+*/
